299bfe6ca09e43a451839a8d65dc13cfe166bb5d
Jared Elliott <bootcamp@jaredclt.com>
2024-02-20 22:30:14 -0500

    getDb -> return result changed to return result.value so it is not an object.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
+  const request = store.put({ id:1, value: content });
   const result = await request;
 };
 
@@ -29,7 +29,7 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
 
-  return result;
+  return result.value; //Convert this from an object to a string
 };
 
 initdb();
